544161eb8f1f00b033d3f52839e1a9b2d687020c
Qya Ahmad <raqayyah@msn.com>
2018-07-31 18:19:12 -0400

    created object for cards and alerts

--- main.js
+++ main.js
@@ -14,16 +14,56 @@
 
 // creating an array to store values of cards
 var cards = [
-  'smith',
-  'smith',
-  'obama',
-  'obama',
-  'hicks',
-  'hicks',
-  'gaines',
-  'gaines',
-  'gossling',
-  'gossling'
+  {
+    couple: 'smith',
+    role: 'wife',
+    cardImage: 'card_image/jadaSmith.jpeg'
+  },
+  {
+    couple: 'smith',
+    role: 'husband',
+    cardImage: 'card_images/willSmith.jpeg'
+  },
+  {
+    couple: 'obama',
+    role: 'wife',
+    cardImage: 'card_images/michelleO.jpeg'
+  },
+  {
+    couple: 'obama',
+    role: 'husband',
+    cardImage: 'card_images/barak.jpg'
+  },
+  {
+    couple: 'hicks',
+    role: 'wife',
+    cardImage: 'card_images/abrahamHicks.jpeg'
+  },
+  {
+    couple: 'hicks',
+    role: 'husband',
+    cardImage: 'card_images/jerry.jpeg'
+  },
+  {
+    couple: 'gaines',
+    role: 'wife',
+    cardImage: 'card_images/joannagaines.jpeg'
+  },
+  {
+    couple: 'gaines',
+    role: 'husband',
+    cardImage: 'card_images/chipgaines.jpeg'
+  },
+  {
+    couple: 'gossling',
+    role: 'wife',
+    cardImage: 'card_images/evamendes.jpeg'
+  },
+  {
+    couple: 'gossling',
+    role: 'husband',
+    cardImage: 'card_images/ryangossling.jpeg'
+  }
 ];
 
 //empty array to hold values of wht is in play
@@ -39,8 +79,10 @@ var checkForMatch = function() {
 };
 // create a function called 'flipCard
 var flipCard = function(cardId) {
-  cardsInPlay.push(cards[cardId]);
-  console.log('You flipped half of the ' + cards[cardId] + ' pair.');
+  cardsInPlay.push(cards[cardId].couple);
+  console.log('You flipped half of the ' + cards[cardId].couple + ' pair.');
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].role);
   // sets the if statement to condition of having two cards
   if (cardsInPlay.length === 2) {
     console.log('Two cards are in play');
@@ -51,3 +93,5 @@ var flipCard = function(cardId) {
 flipCard(0);
 flipCard(2);
 checkForMatch();
+
+//play with the placement of the statments above to get the alerts to fire in the order you prefer
